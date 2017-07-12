4b6ffc41d7d39c4ce35e89ab84386bc24558643c
Henrik Schmidt-Møller <henrikm@tlt.local>
2017-07-12 09:29:04 +0400

    Fixed indentation

--- src/main/java/com/github/jcustenborder/kafka/connect/spooldir/ConfigValidationRules.java
+++ src/main/java/com/github/jcustenborder/kafka/connect/spooldir/ConfigValidationRules.java
@@ -7,77 +7,77 @@ import java.util.Optional;
 import java.util.function.Predicate;
 
 class ConfigValidationRules {
-    private Config config;
+  private Config config;
 
-    ConfigValidationRules(Config config) {
-        this.config = config;
+  ConfigValidationRules(Config config) {
+    this.config = config;
+  }
+
+  <T> When<T> when(String name, T value) {
+    @SuppressWarnings("unchecked") Class<T> type = (Class<T>) value.getClass();
+    return when(name, new Rule<>(type, v -> v.equals(value), "is " + value));
+  }
+
+  <T> When<T> when(String name, Rule<T> rule) {
+    return new When<>(name, rule);
+  }
+
+  <T> ConfigValidationRules validate(String name, Rule<T> rule) {
+    if (!match(name, rule)) {
+      addError(name, rule.description);
+    }
+    return this;
+  }
+
+  private <T> boolean match(String name, Rule<T> rule) {
+    return findValue(name).map(ConfigValue::value).map(v -> rule.type.cast(v)).filter(rule.predicate).isPresent();
+  }
+
+  private Optional<ConfigValue> findValue(String name) {
+    return config.configValues().stream().filter(cv -> cv.name().equals(name)).findFirst();
+  }
+
+  private void addError(String name, String message) {
+    ConfigValue configValue = findValue(name).orElseGet(() -> {
+      ConfigValue cv = new ConfigValue(name);
+      config.configValues().add(cv);
+      return cv;
+    });
+    configValue.addErrorMessage(message);
+  }
+
+  Config getConfig() {
+    return config;
+  }
+
+  static class Rule<T> {
+    private Class<T> type;
+    private Predicate<T> predicate;
+    private String description;
+
+    Rule(Class<T> type, Predicate<T> predicate, String description) {
+      this.type = type;
+      this.predicate = predicate;
+      this.description = description;
+    }
+  }
+
+  class When<S> {
+    private String description;
+
+    When(String name, Rule<S> rule) {
+      this.description = match(name, rule) ? String.format(" when %s %s", name, rule.description) : "";
     }
 
-    <T> When<T> when(String name, T value) {
-        @SuppressWarnings("unchecked") Class<T> type = (Class<T>) value.getClass();
-        return when(name, new Rule<>(type, v -> v.equals(value), "is " + value));
+    <T> When<S> validate(String name, Rule<T> rule) {
+      if (!description.isEmpty()) {
+        ConfigValidationRules.this.validate(name, new Rule<>(
+            rule.type,
+            rule.predicate,
+            String.format("%s%s", rule.description, description))
+        );
+      }
+      return this;
     }
-
-    <T> When<T> when(String name, Rule<T> rule) {
-        return new When<>(name, rule);
-    }
-
-    <T> ConfigValidationRules validate(String name, Rule<T> rule) {
-        if (!match(name, rule)) {
-            addError(name, rule.description);
-        }
-        return this;
-    }
-
-    private <T> boolean match(String name, Rule<T> rule) {
-        return findValue(name).map(ConfigValue::value).map(v -> rule.type.cast(v)).filter(rule.predicate).isPresent();
-    }
-
-    private Optional<ConfigValue> findValue(String name) {
-        return config.configValues().stream().filter(cv -> cv.name().equals(name)).findFirst();
-    }
-
-    private void addError(String name, String message) {
-        ConfigValue configValue = findValue(name).orElseGet(() -> {
-            ConfigValue cv = new ConfigValue(name);
-            config.configValues().add(cv);
-            return cv;
-        });
-        configValue.addErrorMessage(message);
-    }
-
-    Config getConfig() {
-        return config;
-    }
-
-    static class Rule<T> {
-        private Class<T> type;
-        private Predicate<T> predicate;
-        private String description;
-
-        Rule(Class<T> type, Predicate<T> predicate, String description) {
-            this.type = type;
-            this.predicate = predicate;
-            this.description = description;
-        }
-    }
-
-    class When<S> {
-        private String description;
-
-        When(String name, Rule<S> rule) {
-            this.description = match(name, rule) ? String.format(" when %s %s", name, rule.description) : "";
-        }
-
-        <T> When<S> validate(String name, Rule<T> rule) {
-            if (!description.isEmpty()) {
-                ConfigValidationRules.this.validate(name, new Rule<>(
-                        rule.type,
-                        rule.predicate,
-                        String.format("%s%s", rule.description, description))
-                );
-            }
-            return this;
-        }
-    }
+  }
 }
--- src/test/java/com/github/jcustenborder/kafka/connect/spooldir/SpoolDirSourceConnectorConfigTest.java
+++ src/test/java/com/github/jcustenborder/kafka/connect/spooldir/SpoolDirSourceConnectorConfigTest.java
@@ -13,84 +13,84 @@ import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public abstract class SpoolDirSourceConnectorConfigTest {
-    protected Map<String, String> settings;
-    File tempRoot;
-    File inputPath;
-    File finishedPath;
-    File errorPath;
+  protected Map<String, String> settings;
+  File tempRoot;
+  File inputPath;
+  File finishedPath;
+  File errorPath;
 
-    @BeforeEach
-    public void createTempDir() {
-        tempRoot = Files.createTempDir();
-        inputPath = new File(tempRoot, "input");
-        inputPath.mkdirs();
-        finishedPath = new File(tempRoot, "finished");
-        finishedPath.mkdirs();
-        errorPath = new File(tempRoot, "error");
-        errorPath.mkdirs();
+  @BeforeEach
+  public void createTempDir() {
+    tempRoot = Files.createTempDir();
+    inputPath = new File(tempRoot, "input");
+    inputPath.mkdirs();
+    finishedPath = new File(tempRoot, "finished");
+    finishedPath.mkdirs();
+    errorPath = new File(tempRoot, "error");
+    errorPath.mkdirs();
 
-        settings = new LinkedHashMap<>();
-        settings.put(SpoolDirSourceConnectorConfig.INPUT_PATH_CONFIG, inputPath.getAbsolutePath());
-        settings.put(SpoolDirSourceConnectorConfig.FINISHED_PATH_CONFIG, finishedPath.getAbsolutePath());
-        settings.put(SpoolDirSourceConnectorConfig.ERROR_PATH_CONFIG, errorPath.getAbsolutePath());
-        settings.put(SpoolDirSourceConnectorConfig.TOPIC_CONF, "dummy-topic");
-        settings.put(SpoolDirSourceConnectorConfig.INPUT_FILE_PATTERN_CONF, "dummy-pattern");
-        settings.put(SpoolDirSourceConnectorConfig.SCHEMA_GENERATION_ENABLED_CONF, "true");
-    }
+    settings = new LinkedHashMap<>();
+    settings.put(SpoolDirSourceConnectorConfig.INPUT_PATH_CONFIG, inputPath.getAbsolutePath());
+    settings.put(SpoolDirSourceConnectorConfig.FINISHED_PATH_CONFIG, finishedPath.getAbsolutePath());
+    settings.put(SpoolDirSourceConnectorConfig.ERROR_PATH_CONFIG, errorPath.getAbsolutePath());
+    settings.put(SpoolDirSourceConnectorConfig.TOPIC_CONF, "dummy-topic");
+    settings.put(SpoolDirSourceConnectorConfig.INPUT_FILE_PATTERN_CONF, "dummy-pattern");
+    settings.put(SpoolDirSourceConnectorConfig.SCHEMA_GENERATION_ENABLED_CONF, "true");
+  }
 
-    @Test
-    public void undefinedPathsInConfig() {
-        settings.remove(SpoolDirSourceConnectorConfig.INPUT_PATH_CONFIG);
-        SpoolDirSourceConnector connector = createConnector();
+  @Test
+  public void undefinedPathsInConfig() {
+    settings.remove(SpoolDirSourceConnectorConfig.INPUT_PATH_CONFIG);
+    SpoolDirSourceConnector connector = createConnector();
 
-        Config config = connector.validate(settings);
-        ConfigValue inputPathConfig = config.configValues().stream().
-                filter(configValue -> configValue.name().equals(SpoolDirSourceConnectorConfig.INPUT_PATH_CONFIG)).
-                findFirst().get();
-        assertEquals(2, inputPathConfig.errorMessages().size(),
-                "Error message for both 'undefined input path' and 'non-existing directory' should be generated");
-    }
+    Config config = connector.validate(settings);
+    ConfigValue inputPathConfig = config.configValues().stream().
+        filter(configValue -> configValue.name().equals(SpoolDirSourceConnectorConfig.INPUT_PATH_CONFIG)).
+        findFirst().get();
+    assertEquals(2, inputPathConfig.errorMessages().size(),
+        "Error message for both 'undefined input path' and 'non-existing directory' should be generated");
+  }
 
-    @Test
-    public void nonExistingPathsInConfig() {
-        settings.put(SpoolDirSourceConnectorConfig.INPUT_PATH_CONFIG, "nonExisting");
-        SpoolDirSourceConnector connector = createConnector();
+  @Test
+  public void nonExistingPathsInConfig() {
+    settings.put(SpoolDirSourceConnectorConfig.INPUT_PATH_CONFIG, "nonExisting");
+    SpoolDirSourceConnector connector = createConnector();
 
-        Config config = connector.validate(settings);
-        ConfigValue inputPathConfig = config.configValues().stream().
-                filter(configValue -> configValue.name().equals(SpoolDirSourceConnectorConfig.INPUT_PATH_CONFIG)).
-                findFirst().get();
-        assertEquals(1, inputPathConfig.errorMessages().size(),
-                "Error message for 'non-existing directory' should be generated");
-    }
+    Config config = connector.validate(settings);
+    ConfigValue inputPathConfig = config.configValues().stream().
+        filter(configValue -> configValue.name().equals(SpoolDirSourceConnectorConfig.INPUT_PATH_CONFIG)).
+        findFirst().get();
+    assertEquals(1, inputPathConfig.errorMessages().size(),
+        "Error message for 'non-existing directory' should be generated");
+  }
 
-    @Test
-    public void emptyConfig() {
-        settings = new LinkedHashMap<>();
-        SpoolDirSourceConnector connector = createConnector();
-        Config config = connector.validate(settings);
-        Set<String> configValuesWithErrors = config.configValues().stream().
-                filter(configValue -> !configValue.errorMessages().isEmpty()).
-                map(configValue -> configValue.name()).
-                collect(Collectors.toSet());
-        Set<String> expectedConfigValuesWithErrors = new HashSet(Arrays.asList(
-                "input.file.pattern", "finished.path", "topic", "error.path", "input.path", "key.schema", "value.schema"));
-        assertEquals(expectedConfigValuesWithErrors, configValuesWithErrors);
-    }
+  @Test
+  public void emptyConfig() {
+    settings = new LinkedHashMap<>();
+    SpoolDirSourceConnector connector = createConnector();
+    Config config = connector.validate(settings);
+    Set<String> configValuesWithErrors = config.configValues().stream().
+        filter(configValue -> !configValue.errorMessages().isEmpty()).
+        map(configValue -> configValue.name()).
+        collect(Collectors.toSet());
+    Set<String> expectedConfigValuesWithErrors = new HashSet(Arrays.asList(
+        "input.file.pattern", "finished.path", "topic", "error.path", "input.path", "key.schema", "value.schema"));
+    assertEquals(expectedConfigValuesWithErrors, configValuesWithErrors);
+  }
 
-    @Test
-    public void minimalValidConfig() {
-        SpoolDirSourceConnector connector = createConnector();
-        Config config = connector.validate(settings);
-        Set<String> configValuesWithErrors = config.configValues().stream().
-                filter(configValue -> !configValue.errorMessages().isEmpty()).
-                map(configValue -> configValue.name()).
-                collect(Collectors.toSet());
-        Set<String> expectedConfigValuesWithErrors = Collections.emptySet();
-        assertEquals(expectedConfigValuesWithErrors, configValuesWithErrors);
-        //Settings was validated -> possible to create connector based on settings.
-        connector.config(settings);
-    }
+  @Test
+  public void minimalValidConfig() {
+    SpoolDirSourceConnector connector = createConnector();
+    Config config = connector.validate(settings);
+    Set<String> configValuesWithErrors = config.configValues().stream().
+        filter(configValue -> !configValue.errorMessages().isEmpty()).
+        map(configValue -> configValue.name()).
+        collect(Collectors.toSet());
+    Set<String> expectedConfigValuesWithErrors = Collections.emptySet();
+    assertEquals(expectedConfigValuesWithErrors, configValuesWithErrors);
+    //Settings was validated -> possible to create connector based on settings.
+    connector.config(settings);
+  }
 
-    protected abstract SpoolDirSourceConnector createConnector();
+  protected abstract SpoolDirSourceConnector createConnector();
 }
